61e626d33bc2c50f53fed89dc6a23f4b9a60d733
Mike <contact@michaelreid.xyz>
2020-12-15 23:27:25 +0000

    update updateServer logic for new api helper

--- cmcscraper_umbrella/apps/cmcscraper/lib/genservers/update_server.ex
+++ cmcscraper_umbrella/apps/cmcscraper/lib/genservers/update_server.ex
@@ -5,24 +5,9 @@ defmodule Cmcscraper.Genservers.UpdateServer do
     GenServer.start_link(UpdateServerLogic, state)
   end
 
-  def full_update_all_currencies do
-    server_pid()
-    |> GenServer.cast({:op_full_update})
-  end
-
   def update_latest_prices do
     server_pid()
     |> send({:op_update_latest_prices})
-  end
-
-  def historic_map do
-    server_pid()
-    |> GenServer.cast({:op_historic_mapping})
-  end
-
-  def update_coin_list do
-    server_pid()
-    |> GenServer.cast({:op_get_coins_update})
   end
 
   defp server_pid do
--- cmcscraper_umbrella/apps/cmcscraper/lib/genservers/update_server_logic.ex
+++ cmcscraper_umbrella/apps/cmcscraper/lib/genservers/update_server_logic.ex
@@ -4,8 +4,9 @@ defmodule Cmcscraper.Genservers.UpdateServerLogic do
   alias Cmcscraper.RepoFunctions.HistoricPriceRepository
   alias Cmcscraper.RepoFunctions.CurrencyRepository
   alias Cmcscraper.Schemas.Currency
-  alias Cmcscraper.Helpers.FlokiHelper
-  #alias CmcscraperWeb.Endpoint
+  alias Cmcscraper.Schemas.HistoricPrice
+  alias Cmcscraper.Helpers.CmcApiHelper
+  alias Cmcscraper.Models.CmcApi
 
   def start_link(state) do
     GenServer.start_link(__MODULE__, state)
@@ -14,13 +15,12 @@ defmodule Cmcscraper.Genservers.UpdateServerLogic do
   @impl true
   def init(state) do
     Process.register(self(), :update_server)
-    #send(self(), {:price_update_loop})
+    send(self(), {:price_update_loop})
     {:ok, state}
   end
 
   @impl true
   def handle_info({:price_update_loop}, socket) do
-    #Endpoint.broadcast("updatestatus", "update", %{status: "updating latest price info"})
     send(self(), {:op_update_latest_prices})
     Process.send_after(self(), {:price_update_loop}, 30*60*1000)
     {:noreply, socket}
@@ -28,59 +28,12 @@ defmodule Cmcscraper.Genservers.UpdateServerLogic do
 
   @impl true
   def handle_info({:op_update_latest_prices}, socket) do
-    #Endpoint.broadcast("updatestatus", "update", %{status: "updating latest price info"})
     IO.inspect("Updating latest prices")
-    Enum.each(1..3, fn page ->
-      send(self(), {:op_read_page_prices, page})
+    %CmcApi.ListingLatest{} = prices = CmcApiHelper.get_latest_prices(200)
+    Enum.each(prices.data, fn c ->
+      {:ok, %Currency{} = currency} = CurrencyRepository.add_update_currency(Currency.from_object(c))
+      HistoricPriceRepository.add_update_historic_price(%{ HistoricPrice.from_object(c) | currency_id: currency.id })
     end)
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({:op_read_page, page_num}, socket) do
-    IO.inspect("Reading pages")
-    FlokiHelper.get_all_currencies(page_num)
-    |> Enum.each(fn x ->
-      CurrencyRepository.insert_currency(x)
-      #Endpoint.broadcast("updatestatus", "update", %{status: "found " <> x})
-    end)
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({:op_read_page_prices, page_num}, socket) do
-    FlokiHelper.get_all_currencies_prices(page_num)
-    |> Enum.each(fn x ->
-      {name, price, volume, marketcap, ranking} = x
-      HistoricPriceRepository.update_todays_price(name, price, volume, marketcap, ranking)
-      #Endpoint.broadcast("updatestatus", "update", %{status: "found " <> x})
-    end)
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({:op_get_price_data, coin = %Currency{}}, socket) do
-    case FlokiHelper.get_price_history(coin.currency_name) do
-      {:error, _} ->
-        Process.send_after(self(), {:op_get_price_data, coin}, 60000)
-      list -> IO.inspect(list); Enum.map(list, fn x -> HistoricPriceRepository.insert_price(x, coin.id ) end)
-    end
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_info({:op_get_price_data, coin_name}, socket) do
-    #Endpoint.broadcast("updatestatus", "update", %{status: "retrieving price data for " <> coin_name})
-    case FlokiHelper.get_price_history(coin_name) do
-      {:error, _} -> IO.inspect("Error retrieving " <> coin_name)
-      list -> Enum.map(list, fn x -> HistoricPriceRepository.insert_price(x, 2) end)
-    end
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_cast({:op_historic_mapping}, socket) do
-    HistoricPriceRepository.perform_historic_mapping
     {:noreply, socket}
   end
 
