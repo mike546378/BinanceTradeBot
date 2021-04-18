8c41981bb734cdc047a9d712822478aefb034f13
Mike <contact@michaelreid.xyz>
2021-04-18 17:40:21 +0100

    Add total values to bottom of table

--- src/cmcscraper_umbrella/apps/cmcscraper_web/assets/src/components/main_context/main_table/MainTable.tsx
+++ src/cmcscraper_umbrella/apps/cmcscraper_web/assets/src/components/main_context/main_table/MainTable.tsx
@@ -11,29 +11,35 @@ export interface MainTableProps {
 }
 
 export const MainTable: React.FC<MainTableProps> = (props) => {
-    const dummyObj: IPriceData = { date: null, ranking: 9999, price: 999999, volume: 9999, marketcap: 9999 }
+    const dummyObj: IPriceData = { date: null, ranking: 9999, price: 999999, volume: 9999, marketcap: 9999 };
+
     return (
         <HTMLTable striped={true} >
             <thead>
                 <tr>
                     <th>Coin</th>
                     <th>Holding</th>
+                    <th>Peak-Holdings</th>
                     <th>Min-Holdings</th>
-                    <th>Peak-Price</th>
-                    <th>Min-Price</th>
+                    <th>Coins Hodling</th>
                     <th>Sell Percentage</th>
                 </tr>
             </thead>
             <tbody>
                 {props.portfolio
                     .map(x => {
-                        x.currency.priceData = x.currency.priceData.sort((b,a) => Date.parse(a.date) - Date.parse(b.date));
+                        x.currency.priceData = x.currency.priceData.sort((b, a) => Date.parse(a.date) - Date.parse(b.date));
                         if (x.currency.priceData[0] == null) x.currency.priceData.push(dummyObj);
                         return x;
                     })
                     .filter((x) => (x.volume * x.currency.priceData[0]?.price) > 5)
-                    .sort((a,b) => (b.currency.priceData[0].price * b.volume) - (a.currency.priceData[0].price * a.volume))
-                    .map((e) => <MainTableRow key={e.id} {...e} />)}
+                    .sort((a, b) => (b.currency.priceData[0].price * b.volume) - (a.currency.priceData[0].price * a.volume))
+                    .map((e) => <MainTableRow
+                        key={e.id}
+                        {...e} />)}
+                <TotalsTableRow
+                    portfolio={props.portfolio}
+                />
             </tbody>
         </HTMLTable>
     );
@@ -45,6 +51,7 @@ const MainTableRow: React.FC<IPortfolio> = (props) => {
     const peakPrice = props.peakPrice;
     const [sellPercentage, setSellPercentage] = useState(props.percentageChangeRequirement);
     const [sellingAt, setSellingAt] = useState(0);
+
     useEffect(() => {
         setSellingAt(peakPrice - peakPrice / 100 * sellPercentage);
     }, [sellPercentage]);
@@ -57,9 +64,9 @@ const MainTableRow: React.FC<IPortfolio> = (props) => {
         <tr>
             <td>{props.currency?.name}</td>
             <td>${(holding).toFixed(2)} </td>
+            <td>${(peakPrice * props.volume).toFixed(2)} </td>
             <td>${(sellingAt * props.volume).toFixed(2)}</td>
-            <td>${peakPrice} </td>
-            <td>${sellingAt.toFixed(8)} </td>
+            <td>{props.volume} </td>
             <td className={"update-column"}>
                 <NumericInput
                     allowNumericCharactersOnly={true}
@@ -71,5 +78,23 @@ const MainTableRow: React.FC<IPortfolio> = (props) => {
                 <AnchorButton type={"button"} intent={"none"} onClick={updateHandler}>Update</AnchorButton>
             </td>
         </tr>
+    );
+};
+
+const TotalsTableRow: React.FC<MainTableProps> = (props) => {
+
+    const holding = props.portfolio.reduce((sum, x) => sum + x.currency.priceData[0]?.price * x.volume, 0);
+    const peakHolding = props.portfolio.reduce((sum, x) => sum + x.peakPrice * x.volume, 0);
+    const minHolding = props.portfolio.reduce((sum, x) => sum + x.sellingAt * x.volume, 0);
+
+    return (
+        <tr>
+        <td>Totals</td>
+        <td>${(holding).toFixed(2)} </td>
+        <td>${(minHolding).toFixed(2)} </td>
+        <td>${(peakHolding).toFixed(2)}</td>
+        <td />
+        <td />
+    </tr>
     );
 };
